c44ddaeb989286e850eb84ee5e70995363cb33d6
Thxfox-hub <vicg122002@gmail.com>
2026-04-26 20:50:29 +0200

    Fix import path for shipping rates API

--- app/api/shipping/rates/route.js
+++ app/api/shipping/rates/route.js
@@ -1,7 +1,7 @@
 export const dynamic = 'force-dynamic';
 
 // Delegate to the strict implementation to ensure no fallbacks are used.
-import { POST as realPOST } from "../shop/shipping-rates/route";
+import { POST as realPOST } from "../../shop/shipping-rates/route";
 
 export async function POST(req) {
   return realPOST(req);
